7a364024877646b25beb56fefde67710b7829179
shhuang123 <sharonhuang923@gmail.com>
2019-02-04 07:09:53 +0000

    save data and form page

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,5 +1,22 @@
 class StatementsController < ApplicationController
+
   def index
     @statements = Statement.all
   end
+
+  def new
+    @statements = Statement.new
+  end
+
+  def create
+    Statement.create(statement_params)
+    redirect_to root_path
+  end
+
+ private
+
+  def statement_params
+    params.require(:statement).permit(:saying, :author)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'statements#index'
+  resource :statements
 end
